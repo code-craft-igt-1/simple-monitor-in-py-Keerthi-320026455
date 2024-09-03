0bd891aee4bb48c226b0f65a353ff9ba5af4214c
Keerthi K <keerthi.k@philips.com>
2024-09-03 17:11:08 +0530

    Complete the tests

--- check_vitals.py
+++ check_vitals.py
@@ -13,27 +13,15 @@ def blink_warning(message, blink_count=6, blink_interval=1):
         sys.stdout.flush()
         sleep(blink_interval)
 
-def is_temperature_critical(temperature):
-    """Checks if the temperature is outside the normal range."""
-    return temperature > 102 or temperature < 95
-
-def is_pulse_rate_out_of_range(pulseRate):
-    """Checks if the pulse rate is outside the normal range."""
-    return pulseRate < 60 or pulseRate > 100
-
-def is_spo2_low(spo2):
-    """Checks if the oxygen saturation is below the acceptable range."""
-    return spo2 < 90
-
 def vitals_ok(temperature, pulseRate, spo2):
     """Checks vital signs and provides warnings if out of range."""
-    if is_temperature_critical(temperature):
+    if temperature > 102 or temperature < 95:
         blink_warning('Temperature critical!')
         return False
-    if is_pulse_rate_out_of_range(pulseRate):
+    elif pulseRate < 60 or pulseRate > 100:
         blink_warning('Pulse Rate is out of range!')
         return False
-    if is_spo2_low(spo2):
+    elif spo2 < 90:
         blink_warning('Oxygen Saturation out of range!')
         return False
     return True
