3136cb5ecc84574dfc6d0943aa7182610b3797e8
Keerthi K <keerthi.k@philips.com>
2024-09-03 13:04:31 +0530

    Removed duplication and cyclomatic complexity

--- monitor.py
+++ monitor.py
@@ -2,36 +2,26 @@
 from time import sleep
 import sys
 
+def blink_warning(message, blink_count=6, blink_interval=1):
+    """Prints a blinking warning message."""
+    print(message)
+    for _ in range(blink_count):
+        print('\r* ', end='')
+        sys.stdout.flush()
+        sleep(blink_interval)
+        print('\r *', end='')
+        sys.stdout.flush()
+        sleep(blink_interval)
 
 def vitals_ok(temperature, pulseRate, spo2):
-  if temperature > 102 or temperature < 95:
-    print('Temperature critical!')
-    for i in range(6):
-      print('\r* ', end='')
-      sys.stdout.flush()
-      sleep(1)
-      print('\r *', end='')
-      sys.stdout.flush()
-      sleep(1)
-    return False
-  elif pulseRate < 60 or pulseRate > 100:
-    print('Pulse Rate is out of range!')
-    for i in range(6):
-      print('\r* ', end='')
-      sys.stdout.flush()
-      sleep(1)
-      print('\r *', end='')
-      sys.stdout.flush()
-      sleep(1)
-    return False
-  elif spo2 < 90:
-    print('Oxygen Saturation out of range!')
-    for i in range(6):
-      print('\r* ', end='')
-      sys.stdout.flush()
-      sleep(1)
-      print('\r *', end='')
-      sys.stdout.flush()
-      sleep(1)
-    return False
-  return True
+    """Checks vital signs and provides warnings if out of range."""
+    if temperature > 102 or temperature < 95:
+        blink_warning('Temperature critical!')
+        return False
+    elif pulseRate < 60 or pulseRate > 100:
+        blink_warning('Pulse Rate is out of range!')
+        return False
+    elif spo2 < 90:
+        blink_warning('Oxygen Saturation out of range!')
+        return False
+    return True
