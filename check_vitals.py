c7de7c0e50d7fe71785611cbc7cadf1a521939d2
Keerthi K <keerthi.k@philips.com>
2024-09-04 15:06:15 +0530

    Vitals check test

--- check_vitals.py
+++ check_vitals.py
@@ -2,29 +2,42 @@
 from time import sleep
 import sys
 
-def blink_warning(message, blink_count=6, blink_interval=1):
-    """Prints a blinking warning message."""
-    print(message)
-    for _ in range(blink_count):
-        print('\r* ', end='')
-        sys.stdout.flush()
-        sleep(blink_interval)
-        print('\r  ', end='')
-        sys.stdout.flush()
-        sleep(blink_interval)
+class check_vitals:
+  def is_temperature_ok(self, temperature):
+    if temperature in range(95, 102):
+      self.print_wait_time()
+      print('Temperature is normal')
+      return True
+    else:
+      self.print_wait_time()
+      print('Temperature critical!')
+      return False
 
-def check_vital_sign(condition, message):
-    """Checks a condition and blinks a warning if the condition is true."""
-    if condition:
-        blink_warning(message)
-        return False
-    return True
+  def is_pulse_rate_ok(self, pulseRate):
+    if pulseRate in range(60, 100):
+      self.print_wait_time()
+      print('Pulse Rate is normal')
+      return True
+    else:
+      self.print_wait_time()
+      print('Pulse Rate is out of range!')
+      return False
 
-def vitals_ok(temperature, pulseRate, spo2):
-    """Checks vital signs and provides warnings if out of range."""
-    # Check each vital sign and combine results
-    temperature_ok = check_vital_sign(temperature > 102 or temperature < 95, 'Temperature critical!')
-    pulse_rate_ok = check_vital_sign(pulseRate < 60 or pulseRate > 100, 'Pulse Rate is out of range!')
-    spo2_ok = check_vital_sign(spo2 < 90, 'Oxygen Saturation out of range!')
+  def is_spo2_ok(self, spo2):
+    if spo2 < 90:
+      self.print_wait_time()
+      print('Oxygen level is normal')
+      return True
+    else:
+      self.print_wait_time()
+      print('Oxygen Saturation out of range!')
+      return False
 
-    return temperature_ok and pulse_rate_ok and spo2_ok
+  def print_wait_time(self):
+    for i in range(6):
+      print('\r* ', end='')
+      sys.stdout.flush()
+      sleep(1)
+      print('\r *', end='')
+      sys.stdout.flush()
+      sleep(1)
--- monitor.test.py
+++ monitor.test.py
@@ -1,12 +1,21 @@
 import unittest
-from check_vitals import vitals_ok
+from check_vitals import *
 
 
 class MonitorTest(unittest.TestCase):
-    def test_not_ok_when_any_vital_out_of_range(self):
-        self.assertFalse(vitals_ok(99, 102, 70))
-        self.assertTrue(vitals_ok(98.1, 70, 98))
+    monitor = check_vitals()
+    def test_temperature_ok(self):
+        self.assertTrue(self.monitor.is_temperature_ok(100))
+        self.assertFalse(self.monitor.is_temperature_ok(90))
+
+    def test_pulse_rate_ok(self):
+        self.assertTrue(self.monitor.is_pulse_rate_ok(70))
+        self.assertFalse(self.monitor.is_pulse_rate_ok(101))
+
+    def test_spo2_ok(self):
+        self.assertTrue(self.monitor.is_spo2_ok(85))
+        self.assertFalse(self.monitor.is_spo2_ok(92))
 
 
 if __name__ == '__main__':
-  unittest.main()
+    unittest.main()
